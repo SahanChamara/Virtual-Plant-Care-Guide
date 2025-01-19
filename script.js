1bc3cc31613f221d95031871ee85b28d6b34244a
Sahan Chamara <sahanchamara456@gmail.com>
2025-01-19 16:59:30 +0530

    creating ui design

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 let startDetect = document.getElementById("startDetect");
+const video = document.getElementById('video');
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
 
 startDetect.addEventListener('click', function() {
     console.log("click");
-    
+
     
     
     
