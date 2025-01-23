13058454d11d132492c165607163c336ad8d0018
Sahan Chamara <sahanchamara456@gmail.com>
2025-01-23 23:19:08 +0530

    intregrating ai

--- script.js
+++ script.js
@@ -3,10 +3,17 @@ const video = document.getElementById('video');
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 
-startDetect.addEventListener('click', function() {
+startDetect.addEventListener('click', function () {
     console.log("click");
+    // Start video stream
+    navigator.mediaDevices.getUserMedia({ video: true })
+        .then(stream => {
+            video.srcObject = stream;
+            video.play();
+        })
+        .catch(err => console.error('Error accessing webcam:', err));
 
-    
-    
-    
+
+
+
 })
